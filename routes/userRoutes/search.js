e4a28fd6c2d058f0659c2fa0df5551fd5de841b4
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-06-01 04:06:53 +0700

    feat: routes to search recipe by title

--- routes/userRoutes/search.js
+++ routes/userRoutes/search.js
@@ -1,11 +1,27 @@
 var express = require('express')
 var router = express.Router()
+const recipeModel = require('../../models/recipeModel')
+const userModel = require('../../models/userModel')
 const { verifyToken, authorize } = require('../../middlewares/jwt')
 const { cacheMiddleware, myCache } = require('../../middlewares/nodeCache')
 
-router.get('/user/', verifyToken, authorize(['user']), cacheMiddleware, async (req, res, next) => {
-    try {
+router.post('/user/search_title', verifyToken, authorize(['user']), cacheMiddleware, async (req, res, next) => {
+    const userId = req.user.id
+    const {title} = req.body
 
+    try {
+        const checkUserId = await userModel.getUserById(userId)
+        if (checkUserId.length == 0) {
+            return res.status(404).json({ message: 'User not found'})
+        }
+
+        if (req.cacheHit && req.cachedData) {
+            return res.status(200).json({...req.cachedData, cache: 'Cache use' })
+        }
+
+        const rows = await recipeModel.getRecipeByTitle(userId, title)
+        const respon = { rows, cache: 'Cache not use' }
+        myCache.set(req.originalUrl, respon)
         res.status(200).json(respon)
     } catch (e) {
         res.status(500).json({ message: e.message })
