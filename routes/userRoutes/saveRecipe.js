319928f13bf86ec94c7fd1e1057c1ba88976a234
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-06-01 00:45:02 +0700

    feat: routes to get favorite recipe by user login

--- routes/userRoutes/saveRecipe.js
+++ routes/userRoutes/saveRecipe.js
@@ -1,11 +1,20 @@
 var express = require('express')
 var router = express.Router()
+const favoriteModel = require('../../models/favoriteModel')
 const { verifyToken, authorize } = require('../../middlewares/jwt')
 const { cacheMiddleware, myCache } = require('../../middlewares/nodeCache')
 
-router.get('/user/', verifyToken, authorize(['user']), cacheMiddleware, async (req, res, next) => {
-    try {
+router.get('/user/favorite_recipe', verifyToken, authorize(['user']), cacheMiddleware, async (req, res, next) => {
+    const userId = req.user.id
 
+    try {
+        if (req.cacheHit && req.cachedData) {
+            return res.status(200).json({...req.cachedData, cache: 'Cache use' })
+        }
+
+        let rows = await favoriteModel.getFavoriteRecipeById(userId)
+        const respon = { rows, cache: 'Cache not use' }
+        myCache.set(req.originalUrl, respon)
         res.status(200).json(respon)
     } catch (e) {
         res.status(500).json({ message: e.message })
