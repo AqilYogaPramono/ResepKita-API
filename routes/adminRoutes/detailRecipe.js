2cd2138d2c8e5d1780cd897992b9acb9a0d33a47
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-06-04 21:34:26 +0700

    feat: routes to get recipe process

--- routes/adminRoutes/detailRecipe.js
+++ routes/adminRoutes/detailRecipe.js
@@ -14,4 +14,14 @@ router.get('/admin/recipes_approved/:recipeId', verifyToken, authorize(['admin']
     }
 })
 
+router.get('/admin/recipes_processing/:recipeId', verifyToken, authorize(['admin']), async (req, res, next) => {
+    const {recipeId} = req.params
+    try {
+        const detailRecipe = await recipeModel.getDetailRecipeApprovedById(recipeId)
+        res.status(200).json({detailRecipe})
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+})
+
 module.exports = router
