33d4302e02f19a5e8f7c7a8d79d2907e12f680ec
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-06-05 06:06:17 +0700

    fix: adding username admin

--- routes/adminRoutes/dashboard.js
+++ routes/adminRoutes/dashboard.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var router = express.Router()
 const recipeModel = require('../../models/recipeModel')
+const adminModel = require('../../models/adminModel')
 const { verifyToken, authorize } = require('../../middlewares/jwt')
 
 router.get('/admin/dashboard', verifyToken, authorize(['admin']), async (req, res, next) => {
+    const adminId = req.user.id
     try {
         const respon = await recipeModel.dashboardsAdmin()
-
-        res.status(200).json(respon)
+        const admin = await adminModel.getUsernameAdmin(adminId)
+        res.status(200).json({ admin, respon })
     } catch (e) {
         res.status(500).json({ message: e.message })
     }
