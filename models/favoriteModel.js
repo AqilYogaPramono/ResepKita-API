0f2baeb2fbf7e3b28c5838132fa4d46abd6e24fe
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-06-09 14:10:27 +0700

    delete cahche in routes and change to nickanem to get

--- models/favoriteModel.js
+++ models/favoriteModel.js
@@ -3,7 +3,7 @@ const db = require('../configs/db')
 class favoriteModel {
     static getFavoriteRecipeById(userId) {
         return new Promise((resolve, reject) => {
-        db.query(`SELECT r.id, r.title, (SELECT rp.photo_url FROM recipe_photos rp WHERE rp.recipe_id = r.id LIMIT 1) AS photo_url, u.username, COUNT(t.id) AS total_testimonials, CASE WHEN f.id IS NOT NULL THEN 'TRUE' ELSE 'FALSE' END AS is_saved FROM favorites f JOIN recipes r ON f.recipe_id = r.id LEFT JOIN recipe_photos rp ON r.id = rp.recipe_id JOIN users u ON r.user_id = u.id LEFT JOIN testimonials t ON r.id = t.recipe_id WHERE f.user_id = ? GROUP BY r.id, r.title, rp.photo_url, u.username, f.id`, [userId], (err, results) => {
+        db.query(`SELECT r.id, r.title, (SELECT rp.photo_url FROM recipe_photos AS rp WHERE rp.recipe_id = r.id LIMIT 1) AS photo_url, u.id AS user_id, u.nickname, u.photo_profile, COUNT(t.id) AS total_testimonials, CASE WHEN f.id IS NOT NULL THEN 'TRUE' ELSE 'FALSE' END AS is_saved FROM favorites AS f JOIN recipes AS r ON f.recipe_id = r.id JOIN users AS u ON r.user_id = u.id LEFT JOIN testimonials AS t ON r.id = t.recipe_id WHERE f.user_id = ? GROUP BY r.id, r.title, u.id, u.nickname, u.photo_profile, f.id ORDER BY r.id DESC`, [userId], (err, results) => {
             if (err) return reject(err)
             resolve(results)
         })
